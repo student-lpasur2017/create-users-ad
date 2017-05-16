dab76065fcf38466d56f99cd3878e0c3567bec11
zakAsur26 <baptiste.fotia.lpasur16-17@gv5.fr>
2017-05-16 15:27:50 +0200

    Modification du script create-users-ad.ps1

--- create-users-ad.ps1
+++ create-users-ad.ps1
@@ -1,15 +1,22 @@
+# Voir : http://www.supinfo.com/articles/single/342-ajout-utilisateur-active-directory-aide-fichier-csv
 # Import d'un fichier csv dont le delimiteur est un " ; ". On l'importe dans une variable dont va la parcourir plus tard
 $users = import-csv -Path "Chemin/du/fichier.csv" -delimiter ";"
 
 Foreach ($user in $users)
 {
-  $pass =""                          # Definition du mot de passe pour l'user
+  $pass = ""                          # Definition du mot de passe pour l'user
   $nom = $user.firstname             # Definition du nom du user en cherchant dans la var $user et en precisant la colonne avec firstname
   $prenom = $user.lastname           # Definition du prenom du user en cherchant dans la var $user et en precisant la colonne avec lastname
   $ou = $user.ou                     # Definition de l'ou du user en cherchant dans la var $user et en precisant la colonne avec ou
+  $email = $user.email
 
   # Ajout des utilisateurs
-  New-ADuser -name $nom -givename $prenom -Path $ou
+  New-ADuser -name $nom -givename $prenom -Path $ou -EmailAddress $email
 }
 
-echo "Import termine"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "Import termine !"
